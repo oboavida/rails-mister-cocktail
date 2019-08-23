6ccbfe74b0adbd59819de7730b61edeffa5465ff
Antonio Boavida <antonioruela@gmail.com>
2019-08-23 17:59:27 +0200

    MIster Cocktail

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts 'DESTROYING DATABASE!!!'
+
+Cocktail.destroy_all
+Dose.destroy_all
+Ingredient.destroy_all
 
 puts 'Creating new article from seeds...'
 
